6f743c35527519e9ba61d2698e2b2c49c9078538
danelian <141317269+danelian@users.noreply.github.com>
2023-10-19 03:24:03 +0300

    fix pages home & single

--- src/js/app.js
+++ src/js/app.js
@@ -1,90 +1,6 @@
-var myFullpage = new fullpage('#fullpage', {
-	// Navigation
-	// menu: '#header',
-	// lockAnchors: false,
-	// anchors:['firstPage', 'secondPage'],
-	// navigation: false,
-	// navigationPosition: 'right',
-	// navigationTooltips: ['firstSlide', 'secondSlide'],
-	// showActiveTooltip: false,
-	// slidesNavigation: false,
-	// slidesNavPosition: 'bottom',
 
-	// Scrolling
-	// css3: true,
-	// scrollingSpeed: 700,
-	// autoScrolling: true,
-	// fitToSection: true,
-	// fitToSectionDelay: 600,
-	// scrollBar: false,
-	// easing: 'easeInOutCubic',
-	// easingcss3: 'ease',
-	// loopBottom: false,
-	// loopTop: false,
-	// loopHorizontal: true,
-	// continuousVertical: false,
-	// continuousHorizontal: false,
-	// scrollHorizontally: false,
-	// interlockedSlides: false,
-	// dragAndMove: false,
-	// offsetSections: false,
-	// resetSliders: false,
-	// fadingEffect: false,
-	// normalScrollElements: '#element1, .element2',
-	// scrollOverflow: true,
-	// scrollOverflowMacStyle: false,
-	// scrollOverflowReset: false,
-	// touchSensitivity: 15,
-	// bigSectionsDestination: null,
 
-	// Accessibility
-	// keyboardScrolling: true,
-	// animateAnchor: true,
-	// recordHistory: true,
 
-	// Design
-	// controlArrows: true,
-	// controlArrowsHTML: [
-	//     '<div class="fp-arrow"></div>', 
-	//     '<div class="fp-arrow"></div>'
-	// ],
-	// verticalCentered: true,
-	// sectionsColor : ['#ccc', '#fff'],
-	// paddingTop: '3em',
-	// paddingBottom: '10px',
-	// fixedElements: '#header, .footer',
-	// responsiveWidth: 0,
-	// responsiveHeight: 0,
-	// responsiveSlides: false,
-	// parallax: false,
-	// parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-	// dropEffect: false,
-	// dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
-	// waterEffect: false,
-	// waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
-	// cards: false,
-	// cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
-
-	// Custom selectors
-	sectionSelector: '.section',
-	// slideSelector: '.slide',
-
-	// lazyLoading: true,
-	// observer: true,
-	// credits: { enabled: true, label: 'Made with fullPage.js', position: 'right'},
-
-	// Events
-	// beforeLeave: function(origin, destination, direction, trigger){},
-	// onLeave: function(origin, destination, direction, trigger){},
-	// afterLoad: function(origin, destination, direction, trigger){},
-	// afterRender: function(){},
-	// afterResize: function(width, height){},
-	// afterReBuild: function(){},
-	// afterResponsive: function(isResponsive){},
-	// afterSlideLoad: function(section, origin, destination, direction, trigger){},
-	// onSlideLeave: function(section, origin, destination, direction, trigger){},
-	// onScrollOverflow: function(section, slide, position, direction){}
-});
 	
 	// -------------------- BURGER-MENU -------------------------
 	const navMenu = document.getElementById('navMenu'),
@@ -110,24 +26,24 @@ var myFullpage = new fullpage('#fullpage', {
 
 	// -------------------- BURGER-MENU NAV-ACCORDION -------------------------
 	const navlistItems = document.querySelectorAll('.nav__list .menu-item-has-children')
-	navlistItems.forEach((item) => {
-			const navlistHeader = item.querySelector('.btn-reset')
+	navlistItems.forEach((navListItem) => {
+			const navlistHeader = navListItem.querySelector('.btn-reset')
 			navlistHeader.addEventListener('click', () => {
-					const openItem = document.querySelector('.open')
-					toggleNavlistItem(item)
-					if (openItem && openItem !== item) {
-							toggleNavlistItem(openItem)
+					const navListOpenItem = document.querySelector('.open-item')
+					toggleNavlistItem(navListItem)
+					if (navListOpenItem && navListOpenItem !== navListItem) {
+							toggleNavlistItem(navListOpenItem)
 					}
 			})
 	})
-	const toggleNavlistItem = (item) => {
-			const navlistContent = item.querySelector('.sub-menu')
-			if (item.classList.contains('open')) {
+	const toggleNavlistItem = (navListItem) => {
+			const navlistContent = navListItem.querySelector('.sub-menu')
+			if (navListItem.classList.contains('open-item')) {
 					navlistContent.removeAttribute('style')
-					item.classList.remove('open')
+					navListItem.classList.remove('open-item')
 			} else {
 					navlistContent.style.height = navlistContent.scrollHeight + 'px'
-					item.classList.add('open')
+					navListItem.classList.add('open-item')
 			}
 	}
 
@@ -154,8 +70,20 @@ var myFullpage = new fullpage('#fullpage', {
 			item.classList.add('open')
 		}
 	}
-	const firstItemContent = productsItems[0].querySelector('.products__a-content')
-	firstItemContent.style.height = firstItemContent.scrollHeight + 'px'
-	productsItems[0].classList.add('open')
+	if (document.querySelector('.products__accordions')) {
+		const firstItemContent = productsItems[0].querySelector('.products__a-content')
+		firstItemContent.style.height = firstItemContent.scrollHeight + 'px'
+		productsItems[0].classList.add('open')
+	}
 
 	
+	// -------------------- SWIPER -------------------------
+	var swiper = new Swiper(".singleSwiper", {
+		direction: "vertical",
+		slidesPerView: "auto",
+		freeMode: true,
+		scrollbar: {
+			el: ".swiper-scrollbar",
+		},
+		mousewheel: true,
+	});
